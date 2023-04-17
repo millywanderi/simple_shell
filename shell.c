e9dc0799d787ea3393d84a6a4d665ec1b540334e
justin-ipe <justinmwaipe@gmail.com>
2023-04-17 06:31:16 -0700

    Feature: added shell.c file

---
+++ shell.c
@@ -0,0 +1,53 @@
+/*
+ * File: shell.c
+ * Auth: Justin Mwaipe, Millicent Wanderi
+ */
+
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+
+{
+	size_t buffersize = 0;
+	char *buffer = NULL, *buffer_cpy = NULL;
+	pid_t child;
+	int status;
+
+	while (1)
+	{
+	printf("#cisfun$ ");
+	fflush(stdout);
+	int get_line = getline(&buffer, &buffersize, stdin);
+
+	if (get_line == -1)
+		break;
+	buffer[strcspn(buffer, "\n")] = '\0';
+
+	buffer_cpy = strdup(buffer);
+	child = fork();
+	if (child == -1)
+	{
+		perror("Error ");
+		exit(1);
+	}
+	else if (child == 0)
+	{
+		if (execl(buffer, buffer, (char *) NULL) == -1)
+		{
+			perror("./shell ");
+			exit(1);
+		}
+		exit(0);
+	}
+	else
+		waitpid(child, &status, 0);
+	}
+	free(buffer);
+	return (0);
+}
